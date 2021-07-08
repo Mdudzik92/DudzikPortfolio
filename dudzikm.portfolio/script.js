1ec661fcce3a0e413e858f6afc9974178c9912d5
Matt Dudzik <mdudzik92@gmail.com>
2021-07-07 20:59:21 -0500

    Package.json

---
+++ dudzikm.portfolio/script.js
@@ -0,0 +1,12 @@
+// var app = document.getElementById("app");
+
+// app.textContent = "";
+
+// function validateForm() {
+// 	let x = document.forms["contactForm"]["name"].value;
+// 	if (x == "") {
+// 		alert("Name must be filled out");
+// 		return false;
+// 	}
+// }
+
